e9f80a0ae6a9938c5454dce0846524f561651488
Khushwant <khushwantsingh638@gmail.com>
2025-12-26 07:52:46 +0530

    added metadata

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import React from "react";
 
 import ParallaxHero from "../../components/landing-page/ParallaxHero";
 import Sponsorship from "../../components/landing-page/sponsorship";
@@ -10,31 +9,6 @@ import "../globals.css";
 import StatsSection from "../../components/landing-page/StatsSection";
 
 export default function Home(): React.ReactElement {
-  const router = useRouter();
-
-  useEffect(() => {
-    let hasNavigated = false;
-
-    const handleScroll = () => {
-      const scrolledToBottom =
-        window.innerHeight + window.scrollY >= document.body.offsetHeight - 2;
-
-      // if (scrolledToBottom) {
-      //   router.push("/loading");
-      // }
-      const alreadyLoaded = sessionStorage.getItem("hasSeenLoading");
-
-      if (scrolledToBottom && !hasNavigated && !alreadyLoaded) {
-        hasNavigated = true;
-        sessionStorage.setItem("hasSeenLoading", "true");
-        router.push("/loading");
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [router]);
-
   return (
     <main className="min-h-screen bg-black text-white relative">
       {/* Hero Section with Parallax and Timer */}
